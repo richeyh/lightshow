016e1058b6de39bf57829c8a52fe9a0c33baf9b0
Richard Huettel <richard_huettel@homedepot.com>
2016-05-19 17:35:24 +0000

    added configs to set up DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from blueprint import blueprint
 
 
+DB = SQLAlchemy()
 app = Flask(__name__)
+app.config.from_object('app.config')
+DB.init_app(app)
 app.register_blueprint(blueprint)
---
+++ app/config.py
@@ -0,0 +1,4 @@
+TESTING = False
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://user:password@localhost/lightsdb'
+SECRET_KEY = 'random line of text used for encryption'
