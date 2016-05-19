f6bbd8b48d730e184de7fd099fd22bd945ffccc5
Richard Huettel <richard_huettel@homedepot.com>
2016-05-19 17:08:16 +0000

    moved to blue print model to remove imports not at top of files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template, request
-from app import app
 import json
 import serial
+from flask import render_template, request
+from flask.views import MethodView
+from app import app
+
 
 #ser = serial.Serial('/dev/cu.usbmodem1421', 19200)
 ser = 1
@@ -9,51 +11,55 @@ ser = 1
 #{"type": "value", "name": "Brightness", "rangelower": 0, "rangeupper": 255}
 
 modes = [
-	{"name": "Bounce", "opmode": 0, "controls": [
-		{"type": "color", "name": "Color 1"},
-		{"type": "color", "name": "Color 2"}]},
-	{"name": "Rainbow", "opmode": 1, "controls": [
-		{"type": "time", "name": "Speed"}]},
-	{"name": "Flow", "opmode": 2, "controls": [
-		{"type": "color", "name": "Color"},
-		{"type": "time", "name": "Speed"}]},
-	{"name": "Flow 2", "opmode": 3, "controls": [
-		{"type": "color", "name": "Color"},
-		{"type": "time", "name": "Speed"}]},
-	{"name": "Random", "opmode": 4, "controls": [
-		{"type": "time", "name": "Speed"}]},
-	{"name": "Rainbow Flow", "opmode": 5, "controls": [
-		{"type": "time", "name": "Speed"}]},
-	{"name": "Fill", "opmode": 6, "controls": [
-		{"type": "time", "name": "Speed"},
-		{"type": "value", "name": "Subdivisions", "rangelower": 0, "rangeupper": 3}]}
+    {"name": "Bounce", "opmode": 0, "controls": [
+        {"type": "color", "name": "Color 1"},
+        {"type": "color", "name": "Color 2"}]},
+    {"name": "Rainbow", "opmode": 1, "controls": [
+        {"type": "time", "name": "Speed"}]},
+    {"name": "Flow", "opmode": 2, "controls": [
+        {"type": "color", "name": "Color"},
+        {"type": "time", "name": "Speed"}]},
+    {"name": "Flow 2", "opmode": 3, "controls": [
+        {"type": "color", "name": "Color"},
+        {"type": "time", "name": "Speed"}]},
+    {"name": "Random", "opmode": 4, "controls": [
+        {"type": "time", "name": "Speed"}]},
+    {"name": "Rainbow Flow", "opmode": 5, "controls": [
+        {"type": "time", "name": "Speed"}]},
+    {"name": "Fill", "opmode": 6, "controls": [
+        {"type": "time", "name": "Speed"},
+        {"type": "value", "name": "Subdivisions", "rangelower": 0, "rangeupper": 3}]}
 ]
 
 
-@app.route("/", methods=["GET", "POST"])
-@app.route("/index", methods=["GET", "POST"])
-def index():
-    return render_template("index.html", title="Home", modes=modes)
+class IndexView(MethodView):
+
+    def get(self):
+        return render_template("index.html", title="Home", modes=modes)
+
+    def post(self):
+        return render_template("index.html", title="Home", modes=modes)
 
 
-@app.route("/playShow", methods=["GET", "POST"])
-def playShow():
-    requestObject = request.get_json()
-    print requestObject
-    opmode = requestObject["opmode"]
+class PlayView(MethodView):
 
-    message = str(opmode)
-    if "time" in requestObject:
-        message += "S" + requestObject["time"]
-    if "value" in requestObject:
-        message += "S" + requestObject["value"]
-    if "color" in requestObject:
-        message += "S" + requestObject["color"]
+    def post(self):
+        requestObject = request.get_json()
+        print requestObject
+        opmode = requestObject["opmode"]
 
-    message = message.ljust(20, 'X')
-    print message
-    print len(message)
+        message = str(opmode)
+        if "time" in requestObject:
+            message += "S" + requestObject["time"]
+        if "value" in requestObject:
+            message += "S" + requestObject["value"]
+        if "color" in requestObject:
+            message += "S" + requestObject["color"]
 
-    for char in message:
-        ser.write(chr(ord(char)))
-    return json.dumps({"status": "OK"})
+        message = message.ljust(20, 'X')
+        print message
+        print len(message)
+
+        for char in message:
+            ser.write(chr(ord(char)))
+        return json.dumps({"status": "OK"})
